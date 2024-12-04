a1cfce6c5f0e4f6bcb291db3ef95c3f5b26a2f7e
Mark <mark.angyal@grafgroup.com>
2024-12-04 21:14:00 +0100

    encoder

---
+++ Software/GRAF_MNPA/Core/Src/encoder.c
@@ -0,0 +1,34 @@
+/*
+ * encoder.c
+ *
+ *  Created on: Dec 4, 2024
+ *      Author: Mark
+ */
+
+// encoder.c
+#include "encoder.h"
+#include <stm32f7xx_hal_tim.h>
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim5;
+
+// Initialize the Encoders
+void Encoder_Init(void) {
+    // Start TIM2 for A_AXIS
+    HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
+    __HAL_TIM_SET_COUNTER(&htim2, 0); // Reset position to zero
+
+    // Start TIM5 for Z_AXIS
+    HAL_TIM_Encoder_Start(&htim5, TIM_CHANNEL_ALL);
+    __HAL_TIM_SET_COUNTER(&htim5, 0); // Reset position to zero
+}
+
+// Get the position for A_AXIS
+uint32_t Encoder_GetPosition_A_AXIS(void) {
+    return __HAL_TIM_GET_COUNTER(&htim2);
+}
+
+// Get the position for Z_AXIS
+uint32_t Encoder_GetPosition_Z_AXIS(void) {
+    return __HAL_TIM_GET_COUNTER(&htim5);
+}
+
---
+++ Software/GRAF_MNPA/Core/Src/encoder.h
@@ -0,0 +1,18 @@
+/*
+ * encoder.h
+ *
+ *  Created on: Dec 4, 2024
+ *      Author: Mark
+ */
+
+#ifndef SRC_ENCODER_H_
+#define SRC_ENCODER_H_
+#include <main.h>
+#include <stm32f7xx_hal_tim.h>
+
+
+void Encoder_Init(void);
+uint32_t Encoder_GetPosition_A_AXIS(void);
+uint32_t Encoder_GetPosition_Z_AXIS(void);
+
+#endif /* SRC_ENCODER_H_ */
